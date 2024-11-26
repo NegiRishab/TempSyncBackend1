d1664f54d51f64f159b475f464f562ed8508e361
RS <rishabhvicky98@gmail.com>
2024-11-26 19:50:06 +0530

    Selected react and nest for creating a webApp
    - backend development i will use Nest.js
    - frontend development i will use React

---
+++ frontend/platform/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    Hello Rishabh You have to build something
+    </div>
+  );
+}
+
+export default App;
