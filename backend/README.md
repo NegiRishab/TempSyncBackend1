b3196caa9c9ce09f1d66b44225ed0742ed6c6fd3
RS <rishabhvicky98@gmail.com>
2025-04-11 17:27:03 +0530

    Remove frontend and made only backend repo

--- backend/README.md
+++
@@ -1,5 +0,0 @@
-## Migration 
-
-npm run migration:create --name=Addresses  
-npm run build
-npm run migration:run 
--- frontend/next.config.ts
+++
@@ -1,7 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: 'export', // Add this line
-};
-
-export default nextConfig;
