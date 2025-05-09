21cfaad6b4dd4595b88999d2d665bf05eb1bddaa
Rishabh Negi <89731600+NegiRishab@users.noreply.github.com>
2025-05-09 20:03:20 +0530

    Added new changes related to db

--- config/typeorm.config.ts
+++ config/typeorm.config.ts
@@ -1,30 +1,29 @@
-import { DataSource } from 'typeorm';
-import { ConfigService } from '@nestjs/config';
-import { config } from 'dotenv';
-import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
-
+import { DataSource } from "typeorm";
+import { ConfigService } from "@nestjs/config";
+import { config } from "dotenv";
+import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
 
 config();
 
 const configService = new ConfigService();
-const DB_ENTITIES = configService.get<string>('DB_ENTITIES') ?? 'dist/**/*.entity.js';
-const DB_MIGRATIONS = configService.get<string>('DB_MIGRATIONS') ?? 'dist/migrations/*.js';
+const DB_ENTITIES =
+  configService.get<string>("DB_ENTITIES") ?? "dist/**/*.entity.js";
+const DB_MIGRATIONS =
+  configService.get<string>("DB_MIGRATIONS") ?? "dist/migrations/*.js";
 
 let sslConfig = {};
 
-if (process.env.NODE_ENV === 'local') {
-	sslConfig = false;
+if (process.env.NODE_ENV === "local") {
+  sslConfig = false;
 }
 const dataSource: PostgresConnectionOptions = {
-	type: 'postgres',
-	url: configService.get('DB_URL'),
-	logging: configService.get('DB_LOGGING'),
-	entities: [DB_ENTITIES],
-	migrations: [DB_MIGRATIONS],
-	ssl: sslConfig,
-	migrationsTransactionMode: 'each'
+  type: "postgres",
+  url: configService.get("DB_URL"),
+  logging: configService.get("DB_LOGGING"),
+  entities: [DB_ENTITIES],
+  migrations: [DB_MIGRATIONS],
+  ssl: sslConfig,
+  migrationsTransactionMode: "each",
 };
 
-
 module.exports = new DataSource(dataSource);
-
